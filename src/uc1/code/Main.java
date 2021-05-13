e1d5c02362397b67ba8b91be7670811a3ecfb8e8
Aniket-123-bit <anisunny782@gmail.com>
2021-05-13 18:10:30 +0530

    commit Geometry2 push

---
+++ src/uc1/code/Main.java
@@ -0,0 +1,30 @@
+package uc1.code;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // TODO Auto-generated method stub
+        int x1,x2,y1,y2;
+        int a1,a2,b1,b2;
+        double dis1;
+        a1=2;b1=2;a2=3;b2=3;
+        double dis2;
+        x1=1;y1=1;x2=3;y2=3;
+
+        dis1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis1);
+
+        dis2=Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+        System.out.println("distancebetween"+"("+a1+","+b1+"),"+"("+a2+","+b2+")===>"+dis2);
+
+        if (dis1 == dis2) {
+            System.out.println("lines are equals");
+        }
+        else
+            System.out.println("lines are not equals");
+
+    }
+}
+
+
